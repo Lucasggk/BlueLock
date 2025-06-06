294c29297dcce301220110890c643483a3c1e35e
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-06 17:48:01 -0300

    Update Fix name.loader.lua

--- Fix name.loader.lua
+++ Fix name.loader.lua
@@ -1,3 +1,9 @@
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Lucas Hub",
+    Text = "Script Loading",
+    Duration = 2.5
+})
+
 local l = loadstring(game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/release.lua", true))()
 local Window = l:CreateWindow({
     Title = "",
@@ -9,4 +15,12 @@ local Window = l:CreateWindow({
 })
 l:Destroy()
 
-wait(0.05)
+task.wait(0.05)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/BlueLock/refs/heads/main/Lucas_hub.lua"))()
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Lucas Hub",
+    Text = "Script Loaded",
+    Duration = 20
+})
