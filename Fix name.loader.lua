186ae7d867c6e8800446d39b0968163a9ccb4616
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-06 17:38:43 -0300

    Create Fix name.loader.lua

---
+++ Fix name.loader.lua
@@ -0,0 +1,12 @@
+local l = loadstring(game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/release.lua", true))()
+local Window = l:CreateWindow({
+    Title = "",
+    SubTitle = "",
+    TabWidth = 0,
+    Size = UDim2.fromOffset(0, 0),
+    Acrylic = false,
+    Theme = "Dark",
+})
+l:Destroy()
+
+wait(0.05)
