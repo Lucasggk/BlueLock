228743d583eb7a5040be647f1b912ef596ba2537
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-06 18:13:14 -0300

    Update Lucas_hub.lua

--- Lucas_hub.lua
+++ Lucas_hub.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet(""))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/BlueLock/refs/heads/main/Fix.name.ui.lua"))()
 
 local v = {version = "Test V0.01", alpha = true}
 v.alpha = (v.alpha and "Alpha version") or "Release version" -- explicação do and: se a variável antes dele ex. (oi = true) local oi = oi and "x" or "z". se o oi for true ele aceita o primeiro valor caso contrário ele aceita o segundo valor
