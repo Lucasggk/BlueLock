167ff7f5493b4a8856303d572cba48b24ac3df7f
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-08 16:25:41 -0300

    Update Lucas_hub.lua

--- Lucas_hub.lua
+++ Lucas_hub.lua
@@ -83,7 +83,7 @@ main:AddToggle("", {
                     if stamina.Value ~= 100 then
                         stamina.Value = 100
                     end
-                task.wait(0.05)
+                    task.wait(0.05)
                 end
             end)
         end
